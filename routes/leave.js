1700fb9f3759979bd59c6e26e7ace250bb2cab07
NattawatTHwork <nattawatth.work@gmail.com>
2023-12-19 00:18:00 +0700

    curd userleave

--- routes/leave.js
+++ routes/leave.js
@@ -65,29 +65,76 @@ router.post('/create_leave', express.json(), checkUserAuthorization, (req, res, 
 // update leave
 router.put('/update_leave/:id', express.json(), checkUserAuthorization, (req, res, next) => {
     connection.execute(
-        'UPDATE leave_requests SET leave_type = ?, description = ?, start_date = ?, end_date = ?, status = ? WHERE leave_requests_id = ?',
-        [req.body.leave_type, req.body.description, req.body.start_date, req.body.end_date, req.body.status, req.params.id],
-        function (err, results, fields) {
-            if (err) {
-                res.json({ status: 'error', message: err });
-                return;
-            }
-            res.json({ status: 'success' });
-        }
-    );
-});
-
-// delete leave
-router.put('/delete_leave/:id', express.json(), checkUserAuthorization, (req, res, next) => {
-    connection.execute(
-        'UPDATE leave_requests SET deleted = 0 WHERE leave_requests_id = ?',
+        'SELECT status FROM leave_requests WHERE leave_requests_id = ?',
         [req.params.id],
         function (err, results, fields) {
             if (err) {
                 res.json({ status: 'error', message: err });
                 return;
             }
-            res.json({ status: 'success' });
+
+            if (results.length === 0) {
+                res.json({ status: 'nofound', message: 'Leave request not found' });
+                return;
+            }
+
+            const currentStatus = results[0].status;
+
+            if (currentStatus !== 2) {
+                res.json({ status: 'cant_update', message: 'Leave request status is not 2 (Pending)' });
+                return;
+            }
+
+            connection.execute(
+                'UPDATE leave_requests SET leave_type = ?, description = ?, start_date = ?, end_date = ?, status = ? WHERE leave_requests_id = ?',
+                [req.body.leave_type, req.body.description, req.body.start_date, req.body.end_date, req.body.status, req.params.id],
+                function (err, results, fields) {
+                    if (err) {
+                        res.json({ status: 'error', message: err });
+                        return;
+                    }
+                    res.json({ status: 'success' });
+                }
+            );
+        }
+    );
+});
+
+
+// delete leave
+router.put('/delete_leave/:id', express.json(), checkUserAuthorization, (req, res, next) => {
+    connection.execute(
+        'SELECT status FROM leave_requests WHERE leave_requests_id = ?',
+        [req.params.id],
+        function (err, results, fields) {
+            if (err) {
+                res.json({ status: 'error', message: err });
+                return;
+            }
+
+            if (results.length === 0) {
+                res.json({ status: 'nofound', message: 'Leave request not found' });
+                return;
+            }
+
+            const currentStatus = results[0].status;
+
+            if (currentStatus !== 2) {
+                res.json({ status: 'cant_update', message: 'Leave request status is not 2 (Pending)' });
+                return;
+            }
+
+            connection.execute(
+                'UPDATE leave_requests SET deleted = 0 WHERE leave_requests_id = ?',
+                [req.params.id],
+                function (err, results, fields) {
+                    if (err) {
+                        res.json({ status: 'error', message: err });
+                        return;
+                    }
+                    res.json({ status: 'success' });
+                }
+            );
         }
     );
 });
