34338f85b2c7dea83bd2b482d06aec866c2afc9a
NattawatTHwork <nattawatth.work@gmail.com>
2024-11-11 14:38:18 +0700

    20241111

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var cors = require('cors');
+var https = require('https');
+var fs = require('fs');
+require('dotenv').config(); // โหลดค่าจากไฟล์ .env
+
 var app = express();
 
+// สร้างตัวแปร port โดยใช้ค่าจาก .env หากไม่มีใช้ค่าเริ่มต้น 8443
+const port = process.env.PORT || 8443;
+
+// ใช้ CORS
 app.use(cors());
 
+// โหลดใบรับรอง SSL และ private key
+var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/thaicodelab.com/privkey.pem'), // เปลี่ยนเป็น path ของคุณ
+    cert: fs.readFileSync('/etc/letsencrypt/live/thaicodelab.com/fullchain.pem') // เปลี่ยนเป็น path ของคุณ
+};
+
+// เส้นทางทั่วไป
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+// เส้นทางสำหรับ routing อื่นๆ
 app.use('/login', require('./routes/login'));
 app.use('/auth', require('./routes/auth'));
 app.use('/user', require('./routes/user'));
@@ -22,6 +38,7 @@ app.use('/dashboard', require('./routes/dashboard'));
 app.use('/equipment', require('./routes/equipment'));
 app.use('/borrow', require('./routes/borrow'));
 
-app.listen(5000, function () {
-    console.log('CORS-enabled web server listening on port 5000');
+// สร้าง HTTPS server บนพอร์ตตามตัวแปร port
+https.createServer(options, app).listen(port, function () {
+    console.log(`CORS-enabled HTTPS server listening on port ${port}`);
 });
